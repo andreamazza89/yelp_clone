b8c5868ebfa88b2f4cf326e31b8f5f0464c0adb6
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-07-12 16:10:57 +0100

    Implement index of restaurants resource

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,6 @@
 class RestaurantsController < ApplicationController
+  def index
+  end
 
   def show
     @restaurant = Restaurant.find(params[ :id])
@@ -7,14 +9,11 @@ class RestaurantsController < ApplicationController
   def new
   end
 
-
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
     redirect_to @restaurant
   end
-
-
 
   private
   def restaurant_params
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
-feature 'restaurant features' do
-  scenario 'user can add a restaurant with name, description and rating' do
-    visit('/restaurants/new')
-    fill_in('Name', with: "Andrea's Bistro")
-    fill_in('Description', with: "Fantastic")
-    fill_in('Rating', with: '5')
-    click_button('Save Restaurant')
-    expect(page).to have_content("Andrea's Bistro")
+
+feature 'Restaurant features' do
+
+  context 'when no restaurants have been added' do
+
+    scenario 'index page should contain a link to create a new restaurant' do
+      visit('/restaurants')
+      expect(page).to have_content('No restaurants yet, please add one and get reviewing!')
+      expect(page).to have_link('Add a new restaurant')
+    end
+
+    scenario 'user can add a restaurant, then see it displayed on the page' do
+      visit('/restaurants/new')
+      fill_in('Name', with: "Andrea's Bistro")
+      fill_in('Description', with: "Fantastic")
+      fill_in('Rating', with: '5')
+      click_button('Save Restaurant')
+      expect(page).to have_content("Andrea's Bistro")
+    end
   end
 end
