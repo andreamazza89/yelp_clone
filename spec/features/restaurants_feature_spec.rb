03803519513184baf496485c1e6b99386656117a
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-07-12 15:28:46 +0100

    Complete 'adding a restaurant' feature

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 feature 'restaurant features' do
   scenario 'user can add a restaurant with name, description and rating' do
     visit('/restaurants/new')
-    fill_in('name', with: "Andrea's Bistro")
-    fill_in('description', with: "Fantastic")
-    fill_in('rating', with: '5')
+    fill_in('Name', with: "Andrea's Bistro")
+    fill_in('Description', with: "Fantastic")
+    fill_in('Rating', with: '5')
+    click_button('Save Restaurant')
     expect(page).to have_content("Andrea's Bistro")
   end
 end
